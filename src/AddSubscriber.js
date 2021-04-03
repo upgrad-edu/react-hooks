84cc837dbd471caf69aad0f92e6e00abf16f819e
Ayon Ghosh <ayonghosh88@gmail.com>
2021-04-04 00:24:23 +0530

    Add form validation

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -2,7 +2,7 @@ import React, {Component, useState} from 'react';
 import Header from './Header';
 import './AddSubscriber.css';
 import { Link,useHistory } from 'react-router-dom';
-
+import {ValidatorForm,TextValidator} from 'react-material-ui-form-validator'
 export default function AddSubscriber({addSubscriberHandler}){
 
     const [addSubscriberForm,setAddSubscriberForm] = useState({
@@ -25,6 +25,7 @@ export default function AddSubscriber({addSubscriberHandler}){
 
   const  onFormSubmitted = (e) => {
         e.preventDefault();
+
         addSubscriberHandler(addSubscriberForm);
             setAddSubscriberForm({ id: 0, name: '', phone: ' ' });
         history.push("/")
@@ -41,11 +42,45 @@ export default function AddSubscriber({addSubscriberHandler}){
                     <button className="custom-btn">Back</button>
                 </Link>
 
-                <form className="subscriber-form" onSubmit={onFormSubmitted}>
-                    <label htmlFor="name" className="label-control">Name: </label><br />
-                    <input id="name" type="text" className="input-control" name="name" onChange={inputChangedHandler} /><br /><br />
-                    <label htmlFor="phone" className="label-control">Phone: </label><br />
-                    <input id="phone" type="text" className="input-control" name="phone" onChange={inputChangedHandler} /><br /><br />
+                <ValidatorForm className="subscriber-form" onSubmit={onFormSubmitted}>
+
+
+                    <TextValidator
+                        id="name"
+                        label="Enter Name"
+                        type="text"
+                        name="name"
+                        onChange={inputChangedHandler}
+                        value={name}
+                        validators={['required']}
+                        errorMessages={['Name cannot be empty']}
+                    >
+                    </TextValidator>
+
+                    {/*<label htmlFor="name" className="label-control">Name: </label><br />*/}
+                    {/*<input id="name" type="text" className="input-control" name="name" onChange={inputChangedHandler} />*/}
+
+
+
+
+                    <br /><br />
+
+
+                    <TextValidator
+                        id="phone"
+                        type="text"
+                        name="phone"
+                        onChange={inputChangedHandler}
+                        label="Enter PhoneNumber"
+                        value={phone}
+                        validators={['required']}
+                        errorMessages={['Phone Number cannot be empty']}
+                    ></TextValidator>
+                    {/*<label htmlFor="phone" className="label-control">Phone: </label><br />*/}
+                    {/*<input id="phone" type="text" className="input-control" name="phone" onChange={inputChangedHandler} />*/}
+
+
+                    <br /><br />
 
                     <div className="subscriber-info-container">
                         <span className="subscriber-to-add-heading">Subscriber to be added: </span><br />
@@ -54,7 +89,7 @@ export default function AddSubscriber({addSubscriberHandler}){
                     </div>
 
                     <button type="submit" className="custom-btn add-btn">Add</button>
-                </form>
+                </ValidatorForm>
             </div>
         </div>
     )
