0234a878beb3f8f48c33d96728a9689fb1678162
Ayon Ghosh <ayonghosh88@gmail.com>
2021-04-04 00:20:20 +0530

    Add useState hook

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component, useState} from 'react';
 import AddSubscriber from './AddSubscriber';
 import ShowSubscribers from './ShowSubscribers';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-class PhoneDirectory extends Component {
+export default function PhoneDirectory(){
 
-    constructor() {
-        super();
-        this.state = {
-            subscribersList: [
+            const [subscribersList,setSubscribersList] = useState([
                 {
                     id: 1,
                     name: "Shilpa Bhat",
@@ -19,44 +16,106 @@ class PhoneDirectory extends Component {
                     name: "Srishti Gupta",
                     phone: "8888888888"
                 }
-            ]
-        }
-    }
+            ]);
 
-    deleteSubscriberHandler = (subscriberId) => {
-        let subscribersList = this.state.subscribersList;
+
+
+   function deleteSubscriberHandler (subscriberId)  {
+
+
+       console.log("deleteSubscriberHandler called for Subscriber ID" + subscriberId)
         let subscriberIndex = 0;
         subscribersList.forEach(function (subscriber, index) {
             if (subscriber.id === subscriberId) {
                 subscriberIndex = index;
             }
-        }, this);
+        });
+
+       console.log("Index to be deleted " + subscriberIndex)
         let newSubscribers = subscribersList;
         newSubscribers.splice(subscriberIndex, 1);
-        this.setState({subscribers: newSubscribers});
+       console.log("newSubscribers",newSubscribers )
+       setSubscribersList(newSubscribers)
     }
 
-    addSubscriberHandler = (newSubscriber) => {
-        let subscribersList = this.state.subscribersList;
+    function addSubscriberHandler  (newSubscriber) {
+
         if (subscribersList.length > 0) {
             newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
         } else {
             newSubscriber.id = 1;
         }
         subscribersList.push(newSubscriber);
-        this.setState({ subscribersList: subscribersList });
+        setSubscribersList(subscribersList)
+        //this.setState({ subscribersList: subscribersList });
     }
 
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList={this.state.subscribersList} deleteSubscriberHandler={this.deleteSubscriberHandler} />} />
-                    <Route exact path="/add" render={({history}, props) => <AddSubscriber history={history} {...props} addSubscriberHandler={this.addSubscriberHandler} />} />
-                </div>
-            </Router>
-        )
-    }
+
+    return (
+        <Router>
+            <div>
+                <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList={subscribersList} deleteSubscriberHandler={(subscriberId)=>deleteSubscriberHandler(subscriberId)} />} />
+                <Route exact path="/add" render={({history}, props) => <AddSubscriber history={history} {...props} addSubscriberHandler={(newSubscriber)=>addSubscriberHandler(newSubscriber)} />} />
+            </div>
+        </Router>
+    )
+
 }
-
-export default PhoneDirectory;
+//
+// class PhoneDirectory extends Component {
+//
+//     constructor() {
+//         super();
+//         this.state = {
+//             subscribersList: [
+//                 {
+//                     id: 1,
+//                     name: "Shilpa Bhat",
+//                     phone: "9999999999"
+//                 },
+//                 {
+//                     id: 2,
+//                     name: "Srishti Gupta",
+//                     phone: "8888888888"
+//                 }
+//             ]
+//         }
+//     }
+//
+//     deleteSubscriberHandler = (subscriberId) => {
+//         let subscribersList = this.state.subscribersList;
+//         let subscriberIndex = 0;
+//         subscribersList.forEach(function (subscriber, index) {
+//             if (subscriber.id === subscriberId) {
+//                 subscriberIndex = index;
+//             }
+//         }, this);
+//         let newSubscribers = subscribersList;
+//         newSubscribers.splice(subscriberIndex, 1);
+//         this.setState({subscribers: newSubscribers});
+//     }
+//
+//     addSubscriberHandler = (newSubscriber) => {
+//         let subscribersList = this.state.subscribersList;
+//         if (subscribersList.length > 0) {
+//             newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
+//         } else {
+//             newSubscriber.id = 1;
+//         }
+//         subscribersList.push(newSubscriber);
+//         this.setState({ subscribersList: subscribersList });
+//     }
+//
+//     render() {
+//         return (
+//             <Router>
+//                 <div>
+//                     <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList={this.state.subscribersList} deleteSubscriberHandler={this.deleteSubscriberHandler} />} />
+//                     <Route exact path="/add" render={({history}, props) => <AddSubscriber history={history} {...props} addSubscriberHandler={this.addSubscriberHandler} />} />
+//                 </div>
+//             </Router>
+//         )
+//     }
+// }
+//
+// export default PhoneDirectory;
